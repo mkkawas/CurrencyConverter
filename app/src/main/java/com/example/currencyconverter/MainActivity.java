ccefe18d0b81ae6564d6e3b541da6dadae48c772
Mohamad Kawas <mohamad.kawas@lau.edu>
2022-02-04 21:32:39 +0200

    resetting app

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -5,23 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    int operator =0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
     }
-    public void adder() {
-        operator++;
 
-    }
-    public void Swich(){
-        TextView t1 = (TextView)findViewById(t1);
-        if(operator%2 == 0){
-            t1.setText("Dollar to LBP");
-        }else {
-            t1.setText("LBP to Dollar");
-        }
-    }
 }
