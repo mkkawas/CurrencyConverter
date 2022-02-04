f1c43b097e8e6b68c81aab6aa43f9e70533dc39d
Mohamad Kawas <mohamad.kawas@lau.edu>
2022-02-04 22:01:32 +0200

    testing getting text from user

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.currencyconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
+
 
 import android.os.Bundle;
 
@@ -10,6 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public void convert(View v){
+        TextView usd = findViewById(R.id.numberUSD);
+        TextView lbp = findViewById(R.id.numberLBP);
+
+        String usdText = usd.getText().toString();
+        String lbpText = lbp.getText().toString();
+
+        String message= "koosa";
+        if(usdText.equals("")&& lbpText.equals("")){
+            message = "error enter nb ya hmar";
+
+        }
 
     }
 
